b053dbda84bdf02c816f9e4fcbe8747cf6cdc92e
Marcelo do nascimento mayer <107888086+MarceloMayer@users.noreply.github.com>
2023-08-27 13:26:29 -0300

    create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/components/layout/footer/footer/footer.component';
+import { HeaderComponent } from './shared/components/layout/header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
