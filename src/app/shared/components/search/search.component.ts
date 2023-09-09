06a8aa69b0676c8ab87c7c8eeafd7ecd0eb25d06
Marcelo do nascimento mayer <107888086+MarceloMayer@users.noreply.github.com>
2023-09-08 21:52:15 -0300

    base of search componet

---
+++ src/app/shared/components/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent implements OnInit {
+
+  public search:any ={
+    model:""
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public onSubmit():void {
+    event?.preventDefault()
+    console.log(this.search)
+  }
+
+}
+
+
